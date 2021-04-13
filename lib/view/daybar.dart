f0d85317502ba5ee11621f4d3e6db32f83713e2f
ssung2 <479lgs@naver.com>
2021-04-13 23:58:32 +0900

    기억이 잘 ㅡㅡ 힘드네~

--- lib/view/daybar.dart
+++ lib/view/daybar.dart
@@ -1,28 +1,8 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'story.dart';
-import 'package:alram/view/daybar.dart';
+import 'package:alram/story.dart';
 
-void main() {
-  runApp(MyApp());
-}
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: '100억 벌어 룸싸롱 가자',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  TimeOfDay _time = new TimeOfDay.now();
+class MainPage extends StatelessWidget{
 
   List<Story> _stories = [
     Story(
@@ -52,73 +32,16 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('EazyFast Alarm'),
-        centerTitle: true,
-        actions: <Widget>[
-          IconButton(
-            onPressed: () async {
-              final TimeOfDay picked =
-                  await showTimePicker(context: context, initialTime: _time);
-              print(picked.minute);
-              print(picked.hour);
-            },
-            icon: Icon(
-              Icons.alarm,
-              size: 36.0,
-            ),
-          ),
-        ],
+        title: Text('망고를 원 없이 먹자'),
       ),
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Expanded(
-            child: Container(
-              child: ListView(
-                children: <Widget>[
-                  Image.network(
-                      "https://img.buzzfeed.com/thumbnailer-prod-us-east-1/video-api/assets/315473.jpg"),
-                  ListTile(
-                    title: Text('4.2 화면 배치를 위한 위젯'),
-                    onTap: () {
-                      return null;
-                    },
-                  ),
-                  _daybar,
-                  Image.network(
-                      "https://miro.medium.com/max/451/1*mHMr5eihoBMY4NfP-CY2EA.png"),
-                  ListTile(
-                    title: Text('4.2 화면 배치를 위한 위젯'),
-                    onTap: () {
-                      return null;
-                    },
-                  ),
-                  Image.network(
-                      "https://flutter.dev/assets/ui/layout/row-expanded-2-visual-7003ec3a27b38a2cc925b97abfd933fe6d2fce62494400e0e3c9790b715cd0d1.png"),
-                  Image.network(
-                      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7y5ptcpN071_b_7L4_VVJuh9oDri0m03DhQ&usqp=CAU"),
-                  ButtonBar(
-                    buttonMinWidth: 100,
-                    buttonTextTheme: ButtonTextTheme.primary,
-                    buttonHeight: 100,
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget get _daybar => Container(
+      body: Container(
         child: SingleChildScrollView(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.end,
             children: [
               Container(
-                // width: MediaQuery.of(context).size.width,
+                width: MediaQuery.of(context).size.width,
                 margin: EdgeInsets.symmetric(
                   vertical: 10,
                 ),
@@ -168,6 +91,7 @@ class MyHomePage extends StatelessWidget {
             ],
           ),
         ),
-      );
+      ),
+    );
+  }
 }
-
